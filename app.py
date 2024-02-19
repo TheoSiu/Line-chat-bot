afda3ae6e7f4197e7a84f99157e5799043b66067
徐祥修 <siuuu@xuxiangxiudeMacBook-Air.local>
2024-02-20 00:43:25 +0800

    <add notify weather>

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
 import datetime as dt
+import requests
+import json
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -23,10 +26,40 @@ def callback():
     return "OK"
 
 
+
+def notify_weather(today):
+    
+    ## 設定天氣網站的 API 與查詢的城市
+    weather_API = "c78bc673af3701bbd7ac6ccf5f595b9e"
+    city = 'Taipei'
+    url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={weather_API}'
+    
+    ## 爬取天氣網站的資訊
+    r = requests.get(url)
+    data = json.loads(r.text)
+    coll = {}
+    
+    ## 篩選所要的部分資訊
+    for foresct in data['list']:
+        time= dt.datetime.fromtimestamp(foresct['dt']).strftime('%m-%d %H:%M')
+        if time.startswith(today):
+            clock = dt.datetime.fromtimestamp(foresct['dt']).strftime('%H:%M')
+            temp = round(foresct['main']['temp'] - 273.15 , 1)
+            feel_temp = round(foresct['main']['feels_like'] - 273.15 , 1)
+            coll[clock] = (temp, feel_temp)
+
+    selected_times = ['08:00', '14:00', '17:00', '20:00']
+    foresct_temp = pd.DataFrame(coll ).T 
+    foresct_temp.columns = ['目前溫度' , '體感溫度']
+    foresct_temp = foresct_temp.loc[selected_times]
+    return foresct_temp
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    date= str(dt.datetime.today())[0:19]
-    message = TextSendMessage(text= date+ event.message.text)
+    today= dt.datetime.today().strftime('%m-%d')
+
+    message = TextSendMessage(text= notify_weather(today))
     line_bot_api.reply_message(event.reply_token, message)
 
 
