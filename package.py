cc94abc2032fb682ed856192f78756cd8558659c
徐祥修 <siuuu@xuxiangxiudeMacBook-Air.local>
2024-02-20 07:56:51 +0800

    <tt>

--- package.py
+++ package.py
@@ -4,37 +4,43 @@ import json
 import pandas as pd
 
 def notify_weather(today):
-    
-    ## 設定天氣網站的 API 與查詢的城市
     weather_API = "c78bc673af3701bbd7ac6ccf5f595b9e"
     city = 'Taipei'
     url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={weather_API}'
-    
-    ## 爬取天氣網站的資訊
-    r = requests.get(url)
-    # data = json.loads(r.text)
-    foresct_temp = {}
-    
-    ## 篩選所要的部分資訊
-    try :
-        data = json.loads(r.text)
+
+    try:
+        r = requests.get(url)
+        r.raise_for_status()  # 确保请求成功，否则抛出异常
+        data = r.json()  # 直接使用 .json() 将响应内容解析为 JSON 格式
         foresct_temp = {}
-        for foresct in data['list']:
-            time= dt.datetime.fromtimestamp(foresct['dt']).strftime('%m-%d %H:%M')
+
+        for foresct in data.get('list', []):
+            time = dt.datetime.fromtimestamp(foresct['dt']).strftime('%m-%d %H:%M')
             if time.startswith(today):
                 clock = dt.datetime.fromtimestamp(foresct['dt']).strftime('%H:%M')
-                temp = round(foresct['main']['temp'] - 273.15 , 1)
-                feel_temp = round(foresct['main']['feels_like'] - 273.15 , 1)
+                temp = round(foresct['main']['temp'] - 273.15, 1)
+                feel_temp = round(foresct['main']['feels_like'] - 273.15, 1)
                 foresct_temp[clock] = (temp, feel_temp)
             else:
                 break
+
         selected_times = ['08:00', '14:00', '17:00', '20:00']
-    # foresct_temp = pd.DataFrame(coll ).T 
-    # foresct_temp.columns = ['目前溫度' , '體感溫度']
-    # foresct_temp = foresct_temp.loc[selected_times]
         text_weather = '\n'.join([f"{time}: 溫度 {temp}°C, 體感溫度 {feel_temp}°C" for time, (temp, feel_temp) in foresct_temp.items()])
-        print( 'test success or not', text_weather)
+        print('test success or not', text_weather)
         return text_weather
 
-    except:
-        return 'cant get the data'
+    except requests.exceptions.HTTPError as errh:
+        print(f"HTTP Error: {errh}")
+        return 'HTTP Error'
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+        return 'Connection Error'
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error: {errt}")
+        return 'Timeout Error'
+    except requests.exceptions.RequestException as err:
+        print(f"Other Request Error: {err}")
+        return 'Other Request Error'
+    except Exception as e:
+        print(f"Unexpected Error: {e}")
+        return 'Unexpected Error'
