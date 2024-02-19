5f06b7d5fec40cc4588fbe7fa67e90dcbf9b03bc
徐祥修 <siuuu@xuxiangxiudeMacBook-Air.local>
2024-02-20 03:47:48 +0800

    <modift proccfile>

--- app.py
+++ app.py
@@ -60,8 +60,9 @@ def notify_weather(today):
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     today= dt.datetime.today().strftime('%m-%d')
-
-    message = TextSendMessage(text= notify_weather(today))
+    # text_weather= notify_weather(today)
+    text_weather = 'Hello'
+    message = TextSendMessage(text= text_weather)
     line_bot_api.reply_message(event.reply_token, message)
 
 
