8987328a2aa3f8f9c09168199e7d16fb80e3b8f2
徐祥修 <siuuu@xuxiangxiudeMacBook-Air.local>
2024-03-17 01:43:53 +0800

    API val

--- package.py
+++ package.py
@@ -7,10 +7,10 @@ import numpy as np
 import bs4
 from bs4 import BeautifulSoup
 import re
+import os
 
 class Weather:
-    # weather_API = "c78bc673af3701bbd7ac6ccf5f595b9e"
-    weather_API = 'CWA-6F6F89AE-BC6D-4644-BCDB-47912927DE5A'
+    weather_API = os.environ.get('WEATHER_API')
     json_api = {
             "宜蘭縣": "F-D0047-001", "桃園市": "F-D0047-005", "新竹縣": "F-D0047-009", "苗栗縣": "F-D0047-013",
             "彰化縣": "F-D0047-017", "南投縣": "F-D0047-021", "雲林縣": "F-D0047-025", "嘉義縣": "F-D0047-029",
@@ -22,46 +22,7 @@ class Weather:
     con_url = "https://www.cosmopolitan.com/tw/horoscopes/today/"
     Total_Con= ['水瓶座','處女座','獅子座','巨蟹座','雙子座','金牛座',\
                 '牡羊座','天蠍座','射手座','雙魚座','摩羯座','天秤座']
-
-
-    @classmethod
-    def notify_weather(cls, today):
-        # weather_API = "c78bc673af3701bbd7ac6ccf5f595b9e"
-        city = 'Taipei'
-        url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={cls.weather_API}'
-
-        try:
-            r = requests.get(url)
-            r.raise_for_status()  # 确保请求成功，否则抛出异常
-            data = r.json()  # 直接使用 .json() 将响应内容解析为 JSON 格式
-
-            foresct_temp = {}
-
-            for foresct in data.get('list', []):
-                # 使用 UTC 時間
-                utc_time = dt.datetime.utcfromtimestamp(foresct['dt'])
-                
-                # 將 UTC 時間轉換為台灣本地時間
-                taipei_timezone = pytz.timezone('Asia/Taipei')
-                taipei_time = utc_time.replace(tzinfo=pytz.utc).astimezone(taipei_timezone)
-                
-                time = taipei_time.strftime('%m-%d %H:%M')
-                if time.startswith(today):
-                    clock = taipei_time.strftime('%H:%M')
-                    temp = round(foresct['main']['temp'] - 273.15, 1)
-                    feel_temp = round(foresct['main']['feels_like'] - 273.15, 1)
-                    foresct_temp[clock] = (temp, feel_temp)
-                else:
-                    break
-
-            selected_times = ['08:00', '14:00', '17:00', '20:00']
-
-            text_weather = f'天氣預報 {today}:\n' + '\n'.join([f"{time}: 溫度 {temp}°C, 體感溫度 {feel_temp}°C" for time, (temp, feel_temp) in foresct_temp.items()])
-            return text_weather
-        except requests.exceptions.RequestException as e:
-            print(f'Error: {e}')
-            return None
-        
+    
 
     @classmethod
     def Now_weather(cls, address):
